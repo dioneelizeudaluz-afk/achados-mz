f824148cac9c8bcffe0286d6ca259974809e035e
dioneelizeudaluz-afk <dioneelizeudaluz@gmail.com>
2026-08-31 14:26:37 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/layout/Layout'
 import Home from './pages/Home'
 import Search from './pages/Search'
-import Register from './pages/Register'
+import RegisterFound from './pages/RegisterFound'
+import RegisterLost from './pages/RegisterLost'
+import DocumentDetails from './pages/DocumentDetails'
+import AdminLogin from './pages/admin/AdminLogin'
+import AdminDashboard from './pages/admin/AdminDashboard'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div style={{ minHeight: '100vh', background: '#f3f4f6' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="register-found" element={<RegisterFound />} />
+          <Route path="register-lost" element={<RegisterLost />} />
+          <Route path="document/:id" element={<DocumentDetails />} />
+        </Route>
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+      </Routes>
     </BrowserRouter>
   )
-}
+    }
