c0f54d96358b4de39ea9f1a61e317794be748aed
dioneelizeudaluz-afk <dioneelizeudaluz@gmail.com>
2026-08-31 14:29:16 +0200

    Create constants.js

---
+++ achados-mz/src/lib/constants.js
@@ -0,0 +1,33 @@
+export const PROVINCES = [
+  'Maputo Cidade',
+  'Maputo Provincia',
+  'Gaza',
+  'Inhambane',
+  'Sofala',
+  'Manica',
+  'Tete',
+  'Zambezia',
+  'Nampula',
+  'Cabo Delgado',
+  'Niassa'
+]
+
+export const DOCUMENT_TYPES = [
+  { value: 'BI', label: 'Bilhete de Identidade', shortLabel: 'BI' },
+  { value: 'CARTA_CONDUCAO', label: 'Carta de Conducao', shortLabel: 'Carta' },
+  { value: 'PASSAPORTE', label: 'Passaporte', shortLabel: 'Passaporte' }
+]
+
+export const DOCUMENT_STATUS = {
+  PENDENTE: 'Pendente',
+  APROVADO: 'Aprovado',
+  ENCONTRADO: 'Encontrado',
+  DEVOLVIDO: 'Devolvido',
+  REJEITADO: 'Rejeitado',
+  REMOVIDO: 'Removido'
+}
+
+export const LISTING_TYPES = {
+  PERDIDO: 'Perdido',
+  ACHADO: 'Encontrado'
+}
