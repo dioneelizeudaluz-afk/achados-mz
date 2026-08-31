16c55aad5ad2c0ff88040c0cae5527614c5a49f4
dioneelizeudaluz-afk <dioneelizeudaluz@gmail.com>
2026-08-31 13:55:16 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Search from './pages/Search'
+import Register from './pages/Register'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <div style={{ minHeight: '100vh', background: '#f3f4f6' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
